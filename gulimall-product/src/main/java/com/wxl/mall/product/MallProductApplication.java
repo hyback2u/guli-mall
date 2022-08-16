2e4bf12c5a6dc4a9c0a3a01da7b24f7a9b34b6b2
wxlstart <wangxl334752@126.com>
2022-08-16 19:56:50 +0800

    feat:谷粒商城初始化

---
+++ gulimall-product/src/main/java/com/wxl/mall/product/MallProductApplication.java
@@ -0,0 +1,15 @@
+package com.wxl.mall.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author wangxl
+ * @since 2022/8/14 12:53
+ */
+@SpringBootApplication
+public class MallProductApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MallProductApplication.class, args);
+    }
+}
